037429fd42b1287a4c5a20396b9d834fb71ce0d1
Lukasz Babicki <lukaszbabicki@gmail.com>
2018-06-22 15:57:46 +0200

    end new task

---
+++ script.js
@@ -0,0 +1,8 @@
+//task 1//
+const addstrings = (string1 = "hello", string2 = "world");
+console.log(`${string1} ${string2}`);
+
+//task 2//
+const multiply = (number1 = 1, number2 = 1) => number1 + number2;
+console.log(multiply(3));
+console.log(multiply(2, 5));
