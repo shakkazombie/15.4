d200aa65e8fd58f836109d702f28099d42c9e1a4
Lukasz Babicki <lukaszbabicki@gmail.com>
2018-08-23 16:12:45 +0200

    add all tasks

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ console.log(`${string1} ${string2}`);
 const multiply = (number1 = 1, number2 = 1) => number1 + number2;
 console.log(multiply(3));
 console.log(multiply(2, 5));
+
+//task 3//
+
+const average = (...args) => {
+    let sum = 0;
+    args.forEach(arg => {
+        sum += arg;
+    });
+    return (`Średnia podanych liczb wynosi: ${sum / args.length}`);
+};
+
+console.log(average(1, 3, 6, 6));
+console.log(average(1, 3));
+console.log(average(1));
+
+//task 4// 
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
+
+console.log(`Task 4. The average of grades ${grades} is: ${average(...grades)}`);
+
+
+
+
+//Task 5//
+const array1 = [1, 4, 'Krystyna', false, 'Nowak'];
+
+const [, , firstName, , lastName] = array1;
+
+console.log(`The frist name is: ${firstName}`);
+console.log(`The last name is: ${lastName}`);
